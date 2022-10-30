1a994712266bb98603e0af70cfe196a86a9e577e
Thomas <cvejn.tomas@gmail.com>
2022-10-30 17:38:56 +0100

    Updates

---
+++ precache-manifest.876d1bbcec1f63534e40f65dc0dbb939.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "66f37319718c38685312",
+    "url": "/nutsloveproducts/static/css/main.3b97a6b8.chunk.css"
+  },
+  {
+    "revision": "66f37319718c38685312",
+    "url": "/nutsloveproducts/static/js/main.66f37319.chunk.js"
+  },
+  {
+    "revision": "591bdcbf55c446578560",
+    "url": "/nutsloveproducts/static/js/1.591bdcbf.chunk.js"
+  },
+  {
+    "revision": "ac1f4a15efb26dd64d60",
+    "url": "/nutsloveproducts/static/css/2.22afd602.chunk.css"
+  },
+  {
+    "revision": "ac1f4a15efb26dd64d60",
+    "url": "/nutsloveproducts/static/js/2.ac1f4a15.chunk.js"
+  },
+  {
+    "revision": "88d4cca54a70955cd8b3",
+    "url": "/nutsloveproducts/static/js/runtime~main.88d4cca5.js"
+  },
+  {
+    "revision": "3f8b80197b2b91d0d9709959534cc573",
+    "url": "/nutsloveproducts/index.html"
+  }
+];
